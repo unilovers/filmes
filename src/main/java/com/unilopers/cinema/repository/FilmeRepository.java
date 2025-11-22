c73856b386d8f2c0d33c32207342fed3006fbd10
GustavoLambertyCarranza <gumarmota@gmail.com>
2025-11-22 11:03:52 -0300

    :sparkles: Cria interface FilmeRepository estendendo JpaRepository

--- src/main/java/com/unilopers/cinema/repository/FilmeRepository.java
+++ src/main/java/com/unilopers/cinema/repository/FilmeRepository.java
@@ -1,4 +1,12 @@
 package com.unilopers.cinema.repository;
 
-public class FilmeRepository {
+import com.unilopers.cinema.model.Filme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
+    Optional<Filme> findByTitulo(String titulo);
 }
