09d1837e214fb99a2d8f9b2aaba3f7a6f934d95b
GustavoLambertyCarranza <gumarmota@gmail.com>
2025-11-22 11:11:00 -0300

    :sparkles: Cria interface TipoIngressoRepository para acesso a dados

--- src/main/java/com/unilopers/cinema/repository/TipoIngressoRepository.java
+++ src/main/java/com/unilopers/cinema/repository/TipoIngressoRepository.java
@@ -1,4 +1,12 @@
 package com.unilopers.cinema.repository;
 
-public class TipoIngressoM {
+import com.unilopers.cinema.model.TipoIngresso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TipoIngressoRepository extends JpaRepository<TipoIngresso, Long> {
+    Optional<TipoIngresso> findByDescricao(String descricao);
 }
