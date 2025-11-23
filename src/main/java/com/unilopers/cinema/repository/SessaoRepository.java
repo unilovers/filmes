3a8d9d2199e062306759f18f0d41e179e9936b67
Mariana Gabriely <marigaby5457@gmail.com>
2025-11-23 13:20:04 -0300

    :sparkles: Cria interface SessaoRepository para acesso a dados de agendamento

---
+++ src/main/java/com/unilopers/cinema/repository/SessaoRepository.java
@@ -0,0 +1,9 @@
+package com.unilopers.cinema.repository;
+
+import com.unilopers.cinema.model.Sessao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SessaoRepository extends JpaRepository<Sessao, Long> {
+}
